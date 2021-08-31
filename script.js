f28a5aa0f7db9f51576b2cb1152d9f76b54882ec
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-08-31 13:16:26 -0300

    commit

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 //imagem1
 function show_image1(src) {
     var Image_Id = document.getElementById('tela');
-    if (Image_Id.src.match("foto_default.jpg")) {
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto4.jpg") ||Image_Id.src.match("foto2.jpg") || Image_Id.src.match("foto3.jpg")) {
         Image_Id.src = "foto1.jpg";
     }else {
         Image_Id.src = "foto_default.jpg";
     }
 
     var Image_Id_1 = document.getElementById('back');
-    if (Image_Id_1.src.match("foto_default.jpg")) {
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto4.jpg") ||Image_Id_1.src.match("foto2.jpg") || Image_Id_1.src.match("foto3.jpg")) {
         Image_Id_1.src = "foto1.jpg";
     }else {
         Image_Id_1.src = "foto_default.jpg";
     }
 
     var Image_Id_2 = document.getElementById('bolso');
-    if (Image_Id_2.src.match("foto_default.jpg")) {
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto4.jpg") ||Image_Id_2.src.match("foto2.jpg") || Image_Id_2.src.match("foto3.jpg")) {
         Image_Id_2.src = "foto1.jpg";
     }else {
         Image_Id_2.src = "foto_default.jpg";
     }
 
     var Image_Id_3 = document.getElementById('lado');
-    if (Image_Id_3.src.match("foto_default.jpg")) {
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto4.jpg") ||Image_Id_3.src.match("foto2.jpg") || Image_Id_3.src.match("foto3.jpg")) {
         Image_Id_3.src = "foto1.jpg";
     }else {
         Image_Id_3.src = "foto_default.jpg";
     }
 
     var Image_Id_4 = document.getElementById('total');
-    if (Image_Id_4.src.match("foto_default.jpg")) {
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto4.jpg") ||Image_Id_4.src.match("foto2.jpg") || Image_Id_4.src.match("foto3.jpg")) {
         Image_Id_4.src = "foto1.jpg";
     }else {
         Image_Id_4.src = "foto_default.jpg";
@@ -39,35 +39,35 @@ function show_image1(src) {
 
 function show_image2(src){
     var Image_Id = document.getElementById('tela');
-    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ) {
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ||Image_Id.src.match("foto4.jpg") || Image_Id.src.match("foto3.jpg") ) {
         Image_Id.src = "foto2.jpg";
     }else {
         Image_Id.src = "foto_default.jpg";
     }
 
     var Image_Id_1 = document.getElementById('back');
-    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg")) {
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_1.src.match("foto4.jpg") || Image_Id_1.src.match("foto3.jpg")) {
         Image_Id_1.src = "foto2.jpg";
     }else {
         Image_Id_1.src = "foto_default.jpg";
     }
 
     var Image_Id_2 = document.getElementById('bolso');
-    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg")) {
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg") ||Image_Id_2.src.match("foto4.jpg") || Image_Id_2.src.match("foto3.jpg")) {
         Image_Id_2.src = "foto2.jpg";
     }else {
         Image_Id_2.src = "foto_default.jpg";
     }
 
     var Image_Id_3 = document.getElementById('lado');
-    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_3.src.match("foto1.jpg")) {
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_3.src.match("foto4.jpg") || Image_Id_3.src.match("foto3.jpg")) {
         Image_Id_3.src = "foto2.jpg";
     }else {
         Image_Id_3.src = "foto_default.jpg";
     }
 
     var Image_Id_4 = document.getElementById('total');
-    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg")) {
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg") ||Image_Id_4.src.match("foto4.jpg") || Image_Id_4.src.match("foto3.jpg")) {
         Image_Id_4.src = "foto2.jpg";
     }else {
         Image_Id_4.src = "foto_default.jpg";
@@ -76,35 +76,35 @@ function show_image2(src){
 
 function show_image3(src){
     var Image_Id = document.getElementById('tela');
-    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ||Image_Id.src.match("foto2.jpg")) {
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ||Image_Id.src.match("foto2.jpg")||Image_Id.src.match("foto4.jpg")) {
         Image_Id.src = "foto3.jpg";
     }else {
         Image_Id.src = "foto_default.jpg";
     }  
 
     var Image_Id_1 = document.getElementById('back');
-    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_1.src.match("foto2.jpg")) {
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_1.src.match("foto2.jpg")||Image_Id_1.src.match("foto4.jpg")) {
         Image_Id_1.src = "foto3.jpg";
     }else {
         Image_Id_1.src = "foto_default.jpg";
     } 
 
     var Image_Id_2 = document.getElementById('bolso');
-    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg") ||Image_Id_2.src.match("foto2.jpg")) {
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg") ||Image_Id_2.src.match("foto2.jpg")||Image_Id_2.src.match("foto4.jpg")) {
         Image_Id_2.src = "foto3.jpg";
     }else {
         Image_Id_2.src = "foto_default.jpg";
     } 
 
     var Image_Id_3 = document.getElementById('lado');
-    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_3.src.match("foto1.jpg") ||Image_Id_3.src.match("foto2.jpg")) {
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_3.src.match("foto1.jpg") ||Image_Id_3.src.match("foto2.jpg")||Image_Id_3.src.match("foto4.jpg")) {
         Image_Id_3.src = "foto3.jpg";
     }else {
         Image_Id_3.src = "foto_default.jpg";
     }
 
     var Image_Id_4 = document.getElementById('total');
-    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg") ||Image_Id_4.src.match("foto2.jpg")) {
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg") ||Image_Id_4.src.match("foto2.jpg")||Image_Id_4.src.match("foto4.jpg")) {
         Image_Id_4.src = "foto3.jpg";
     }else {
         Image_Id_4.src = "foto_default.jpg";
