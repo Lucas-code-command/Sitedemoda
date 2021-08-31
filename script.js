55504855d1eeccf51a3faa597012eea7d62a99e1
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-08-31 13:07:33 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//imagem1
 function show_image1(src) {
     var Image_Id = document.getElementById('tela');
     if (Image_Id.src.match("foto_default.jpg")) {
@@ -34,4 +35,115 @@ function show_image1(src) {
         Image_Id_4.src = "foto_default.jpg";
     }
 
+}
+
+function show_image2(src){
+    var Image_Id = document.getElementById('tela');
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ) {
+        Image_Id.src = "foto2.jpg";
+    }else {
+        Image_Id.src = "foto_default.jpg";
+    }
+
+    var Image_Id_1 = document.getElementById('back');
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg")) {
+        Image_Id_1.src = "foto2.jpg";
+    }else {
+        Image_Id_1.src = "foto_default.jpg";
+    }
+
+    var Image_Id_2 = document.getElementById('bolso');
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg")) {
+        Image_Id_2.src = "foto2.jpg";
+    }else {
+        Image_Id_2.src = "foto_default.jpg";
+    }
+
+    var Image_Id_3 = document.getElementById('lado');
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_3.src.match("foto1.jpg")) {
+        Image_Id_3.src = "foto2.jpg";
+    }else {
+        Image_Id_3.src = "foto_default.jpg";
+    }
+
+    var Image_Id_4 = document.getElementById('total');
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg")) {
+        Image_Id_4.src = "foto2.jpg";
+    }else {
+        Image_Id_4.src = "foto_default.jpg";
+    }
+}
+
+function show_image3(src){
+    var Image_Id = document.getElementById('tela');
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ||Image_Id.src.match("foto2.jpg")) {
+        Image_Id.src = "foto3.jpg";
+    }else {
+        Image_Id.src = "foto_default.jpg";
+    }  
+
+    var Image_Id_1 = document.getElementById('back');
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_1.src.match("foto2.jpg")) {
+        Image_Id_1.src = "foto3.jpg";
+    }else {
+        Image_Id_1.src = "foto_default.jpg";
+    } 
+
+    var Image_Id_2 = document.getElementById('bolso');
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg") ||Image_Id_2.src.match("foto2.jpg")) {
+        Image_Id_2.src = "foto3.jpg";
+    }else {
+        Image_Id_2.src = "foto_default.jpg";
+    } 
+
+    var Image_Id_3 = document.getElementById('lado');
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_3.src.match("foto1.jpg") ||Image_Id_3.src.match("foto2.jpg")) {
+        Image_Id_3.src = "foto3.jpg";
+    }else {
+        Image_Id_3.src = "foto_default.jpg";
+    }
+
+    var Image_Id_4 = document.getElementById('total');
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg") ||Image_Id_4.src.match("foto2.jpg")) {
+        Image_Id_4.src = "foto3.jpg";
+    }else {
+        Image_Id_4.src = "foto_default.jpg";
+    }
+}
+
+function show_image4(src){
+    var Image_Id = document.getElementById('tela');
+    if (Image_Id.src.match("foto_default.jpg") ||Image_Id.src.match("foto1.jpg") ||Image_Id.src.match("foto2.jpg") || Image_Id.src.match("foto3.jpg")) {
+        Image_Id.src = "foto4.jpg";
+    }else {
+        Image_Id.src = "foto_default.jpg";
+    }   
+
+    var Image_Id_1 = document.getElementById('back');
+    if (Image_Id_1.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_1.src.match("foto2.jpg") || Image_Id_1.src.match("foto3.jpg")) {
+        Image_Id_1.src = "foto4.jpg";
+    }else {
+        Image_Id_1.src = "foto_default.jpg";
+    }  
+
+    var Image_Id_2 = document.getElementById('bolso');
+    if (Image_Id_2.src.match("foto_default.jpg") ||Image_Id_2.src.match("foto1.jpg") ||Image_Id_2.src.match("foto2.jpg") || Image_Id_2.src.match("foto3.jpg")) {
+        Image_Id_2.src = "foto4.jpg";
+    }else {
+        Image_Id_2.src = "foto_default.jpg";
+    }  
+
+    var Image_Id_3 = document.getElementById('lado');
+    if (Image_Id_3.src.match("foto_default.jpg") ||Image_Id_1.src.match("foto1.jpg") ||Image_Id_3.src.match("foto2.jpg") || Image_Id_3.src.match("foto3.jpg")) {
+        Image_Id_3.src = "foto4.jpg";
+    }else {
+        Image_Id_3.src = "foto_default.jpg";
+    }  
+
+    var Image_Id_4 = document.getElementById('total');
+    if (Image_Id_4.src.match("foto_default.jpg") ||Image_Id_4.src.match("foto1.jpg") ||Image_Id_4.src.match("foto2.jpg") || Image_Id_4.src.match("foto3.jpg")) {
+        Image_Id_4.src = "foto4.jpg";
+    }else {
+        Image_Id_4.src = "foto_default.jpg";
+    }  
 }
